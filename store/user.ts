6eb79b5dc0bc783c56121237f810335b8a0676d1
Nusratullo Akhunov <nusratulloakhunov@Nusratullos-iMac.local>
2024-11-06 12:55:20 +0500

    dsfdsf

--- store/user.ts
+++ store/user.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 
 export const useUserStore = defineStore('user', {
     state: () => ({
-        isLoggedIn: true, // Default to false, set this based on actual auth logic
+        isLoggedIn: false, // Default to false, set this based on actual auth logic
     }),
     actions: {
         login() {
